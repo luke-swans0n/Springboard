8423e6842f2f5c1c0f25f71a5a051dbcb51d378e
The_Administrator <mr.luke.swanson@gmail.com>
2021-08-17 22:18:49 -0500

    adding_api
    adding_api_project

---
+++ IN_PROGRESS/intradaymodelling/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Modelling prices and volatility of intraday financial data',
+    author='Luke Swanson',
+    license='MIT',
+)
